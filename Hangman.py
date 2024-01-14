4e1b004e5389b53c4b91d726dad56edcbb3c8b11
pilcek8181 <patrikzacek2202@gmail.com>
2024-01-14 21:52:52 +0100

    two moving blocks

--- Hangman.py
+++ Hangman.py
@@ -9,15 +9,19 @@ screenHeight = 1200
 screen = pygame.display.set_mode((screenWidth,screenHeight))
 
 player = pygame.Rect(300,250,50,50)
+player2 = pygame.Rect(300,250,50,50)
 
 #GAME LOOP
 run = True
 while run:
 
-    screen.fill((0,0,0))
+    screen.fill((255,255,255))
     pygame.draw.rect(screen,(255,0,0),player)
+    pygame.draw.rect(screen,(0,255,0),player2)
+
     key = pygame.key.get_pressed()
-    
+    pygame.draw.circle(screen,(12,34,23),(600,600),30)
+
     if key[pygame.K_a] == True:
         player.move_ip(-1,0)
     elif key[pygame.K_d] == True:
@@ -26,6 +30,16 @@ while run:
         player.move_ip(0,-1)
     elif key[pygame.K_s] == True:
         player.move_ip(0,1)
+
+    ##elif key[pygame.K_SPACE] == True:
+    if key[pygame.K_LEFT] == True:
+        player2.move_ip(-1,0)
+    elif key[pygame.K_RIGHT] == True:
+        player2.move_ip(1,0)
+    elif key[pygame.K_UP] == True:
+        player2.move_ip(0,-1)
+    elif key[pygame.K_DOWN] == True:
+        player2.move_ip(0,1)    
     
     
     #EVENT HANDLER
