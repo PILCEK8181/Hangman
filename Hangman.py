d9c0a81ff8d30ea85216fed6c8081f230d1f6e9f
pilcek8181 <patrikzacek2202@gmail.com>
2024-01-14 20:24:53 +0100

    red moving block

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,39 @@
-#Hi
+import pygame
+
+pygame.init()
+
+#SCREEN
+screenWidth = 1200
+screenHeight = 1200
+
+screen = pygame.display.set_mode((screenWidth,screenHeight))
+
+player = pygame.Rect(300,250,50,50)
+
+#GAME LOOP
+run = True
+while run:
+
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen,(255,0,0),player)
+    key = pygame.key.get_pressed()
+    
+    if key[pygame.K_a] == True:
+        player.move_ip(-1,0)
+    elif key[pygame.K_d] == True:
+        player.move_ip(1,0)
+    elif key[pygame.K_w] == True:
+        player.move_ip(0,-1)
+    elif key[pygame.K_s] == True:
+        player.move_ip(0,1)
+    
+    
+    #EVENT HANDLER
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+
+pygame.quit()
